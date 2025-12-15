1f55c5d3c1967a8cbb03538830fd68db73528420
Frujter <sebastianfrujter@gmail.com>
2025-12-15 14:49:55 -0300

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -3,17 +3,33 @@
 volatile num = 0;
 
 int main(){
-    d7s_init;
+    
     NVIC_EnableIRQ(PORTA_IRQ2);//habilito interrupciones
     PORTA -> PCR[2] |= (1 << 8); 
     PTA -> PDDR |= (1u << 2);//Defino los pines de las interrupciones como entradas
-    PORTA -> PCR[2] = PORT_PCR_MUX(1) | PORTC_PCR_IRQC(0b1101);//Seteo la interrupción para el flanco de subida
     NVIC_SetPriority(PORTA_IRQ2,0); //n valor del 0 al 3 siendo 0 el de más prioridad.
+    PORTA -> PCR[2] = PORT_PCR_MUX(1) | PORTC_PCR_IRQC(0b1101);//Seteo la interrupción para el flanco de subida
+    d7s_init();
 
-}
-while (1){
-    
+    while (1){
+    bool mas = false;
+    if (PORTA->ISFR & (1 << 2)){
+        if ((num < 10)&&(!mas)){
+            num++
+            if(num==9){
+                mas = true;
+            }
+        }else{
+            num--;
+        }
+        if ((num)==0){
+            mas = false;
+        }     
+    }
    
 
 }
+}
+
+
 
